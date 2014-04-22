5ee4545640fa1449094a20b2cb95078cc76f5e0c
EAC69 <EAC69@localhost>
2014-04-22 10:50:34 -0400

    History on the heap; testing.

--- include/Board.h
+++ include/Board.h
@@ -5,6 +5,7 @@
 
 #include <random>
 #include <vector>
+#include <memory>
 
 #include "Defs.h"
 #include "Response.h"
@@ -36,16 +37,17 @@ class Board
 		static const constexpr U8 newTile[10] = {1, 1, 1, 1, 1, 1, 1, 2, 1, 1};
 		static const constexpr char* printString[12] = {"    ", " 2  ", " 4  ", " 8  ", " 16 ", " 32 ", " 64 ", " 128", "256 ", "512 ", "1024", "2048"};
 
-		std::vector<std::vector<U8>> history = {std::vector<U8>(16)};
-		U8* board = &history.back()[0];
+		std::vector<std::vector<U8>>* history_ = new std::vector<std::vector<U8>>;
+		std::vector<std::vector<U8>>& history = *history_;
+		U8* board;
 
 		std::vector<U8> getEmptySquares();
 		U8 getHighestTile();
 
-		inline bool isEmpty(U8 sq) { return board[sq] == 0; }
+		inline bool isEmpty(U8 sq) { return (board[sq] == 0); }
 		inline void moveTile(U8 sSq, U8 tSq) { board[tSq] = board[sSq]; board[sSq] = 0; }
 		inline void moveIfAble(U8 sSq, U8 tSq) { if (isEmpty(tSq)) moveTile(sSq, tSq); }
-		inline bool isMergeable(U8 sSq, U8 tSq) { return (board[sSq] == board[tSq]) * board[sSq]; }
+		inline bool isMergeable(U8 sSq, U8 tSq) { return ((board[sSq] == board[tSq]) && board[sSq] != 0); }
 		inline void mergeTiles(U8 sSq, U8 tSq) { board[tSq]++; board[sSq] = 0; }
 		inline void mergeIfAbleAA(U8 sSq, U8 tSq, U8& sq, S8 num) { if (isMergeable(sSq, tSq)) { mergeTiles(sSq, tSq); sq += num; } }
 
@@ -66,7 +68,8 @@ class Board
 		bool isFull();
 		bool areNoMerges();
 	public:
-		Board() : engine(std::mt19937(seed__)), tileDistribution(std::uniform_int_distribution<U8>(0_U8, 9_U8)) { respond(); respond(); }
+		Board() : engine(std::mt19937(seed__)), tileDistribution(std::uniform_int_distribution<U8>(0_U8, 9_U8)) { history.push_back(std::vector<U8>(16)); updateBoardPointer(); respond(); respond(); }
+		~Board() { delete history_; }
 
 		static inline constexpr U8 getSqIndex(U8 row, U8 col) { return (col * 4) + row; }
 
